ca4da837ee24819bf72195b357d1dafd0f68e7d8
SCPC 518 <alesuarezj2004@gmail.com>
2024-12-23 20:58:56 -0400

    VitavinumDW2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { BebidasComponent } from './pages/bebidas/bebidas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },  
+    { path: 'acercade', component: AcercadeComponent }, 
+    { path: 'bebidas', component: BebidasComponent},
+    { path: '**', component: InicioComponent }, 
+  
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { PipesComponent } from './pages/pipes/pipes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BebidasComponent } from './pages/bebidas/bebidas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent,
+    PipesComponent,
+    NavbarComponent,
+    FooterComponent,
+    BebidasComponent
   ],
   imports: [
     BrowserModule,
